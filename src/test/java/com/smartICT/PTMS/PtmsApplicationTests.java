aef7cad9c5433712c46809153da4b38c8c2c1c4a
Ömer Faruk Sağlam <omrfrksglm06@gmail.com>
2023-06-22 15:20:33 +0300

    initial commit

---
+++ src/test/java/com/smartICT/PTMS/PtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartICT.PTMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
